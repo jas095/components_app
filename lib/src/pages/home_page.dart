0bb5ad8ea646fdad8df78565a36b1797199110fd
Jose Angel <jas@deb>
2020-08-02 08:06:59 -0400

    Practical use of Future Builder and List

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:components_app/src/providers/menu_provider.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,24 +15,43 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _list() {
-    return ListView(
-      children: _listItems(),
+    // print(menuProvider.options);
+    // menuProvider.loadData();
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listItems(snapshot.data),
+        );
+      },
     );
+
+    // return ListView(
+    //   children: _listItems(),
+    // );
   }
 
-  List<Widget> _listItems() {
-    return [
-      ListTile(
-        title: Text('No body says it was easy!'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('No body says it was easy!'),
-      ),
-      Divider(),
-      ListTile(
-        title: Text('No body says it was easy!'),
-      ),
-    ];
+  List<Widget> _listItems(List<dynamic> data) {
+    final List<Widget> options = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.android,
+          color: Colors.green,
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.amber,
+        ),
+        onTap: () {},
+      );
+
+      options..add(widgetTemp)..add(Divider());
+    });
+
+    return options;
   }
 }
