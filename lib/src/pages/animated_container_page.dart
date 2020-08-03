7a26cdec2dbed48be4513573d682215eba8f6044
Jose Angel <jas@deb>
2020-08-03 18:11:47 -0400

    Animate the properties of a container

---
+++ lib/src/pages/animated_container_page.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class AnimatedContainerPage extends StatefulWidget {
+  AnimatedContainerPage({Key key}) : super(key: key);
+
+  @override
+  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.redAccent;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Animated Container'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          height: _height,
+          width: _width,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          // Define how long the animation should take.
+          duration: Duration(seconds: 1),
+          // Provide an optional curve to make the animation feel smoother.
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: _changeAnimation,
+        child: Icon(Icons.play_circle_filled),
+      ),
+    );
+  }
+
+  void _changeAnimation() {
+    final random = Random();
+    setState(() {
+      _width = random.nextInt(250).toDouble();
+      _height = random.nextInt(300).toDouble();
+      _color = Color.fromARGB(
+          random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+    });
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:components_app/src/pages/home_page.dart';
 import 'package:components_app/src/pages/alert_page.dart';
 import 'package:components_app/src/pages/cards_page.dart';
 import 'package:components_app/src/pages/avatar_page.dart';
+import 'package:components_app/src/pages/animated_container_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -11,5 +12,6 @@ Map<String, WidgetBuilder> getAplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
   };
 }
