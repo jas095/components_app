b8af04075dc2fa62a6d2297b170dd72f22b657e7
Jose Angel <jas@deb>
2020-08-03 16:11:56 -0400

    Practical use of CircleAvatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -9,6 +9,32 @@ class AvatarPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Avatar Page'),
         backgroundColor: Colors.green,
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(3.3),
+            child: CircleAvatar(
+              // child: Text('KL'),
+              //backgroundColor: Colors.black45,
+              backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbvzWK4FzUYpoKiq0SVpc3kq5ox1x4rPufbg&usqp=CAU'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('KL'),
+              backgroundColor: Colors.black45,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage.assetNetwork(
+          placeholder: 'assets/jar-loading.gif',
+          image:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbvzWK4FzUYpoKiq0SVpc3kq5ox1x4rPufbg&usqp=CAU',
+        ),
       ),
       backgroundColor: Colors.blueGrey[50],
       floatingActionButton: FloatingActionButton(
