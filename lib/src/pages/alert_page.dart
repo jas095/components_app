365d36fd7d35f027739e3cb42442e0eec1efbe5e
Jose Angel <jas@deb>
2020-08-03 14:16:09 -0400

    Show an alert that display a message in a dialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -10,6 +10,14 @@ class AlertPage extends StatelessWidget {
         title: Text('Alert Page'),
         backgroundColor: Colors.green,
       ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Show an alert!'),
+            color: Colors.green,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+            onPressed: () => _showAlert(context)),
+      ),
       backgroundColor: Colors.blueGrey[50],
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.keyboard_backspace),
@@ -18,6 +26,41 @@ class AlertPage extends StatelessWidget {
         },
         backgroundColor: Colors.green,
       ),
+    );
+  }
+
+  _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+          title: Text('Great alert!'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                  'Well done, you do it! \nNow you can write a lot of thing and show an amazing text here.\n'),
+              FlutterLogo(
+                size: 100.0,
+                textColor: Colors.lightGreen,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancel')),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Okay!'))
+          ],
+        );
+      },
     );
   }
 }
