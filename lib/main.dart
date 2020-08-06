2a8ccc1004f8090a4f8839fbb9584b5153653241
Jose Angel <jas@deb>
2020-08-05 23:32:38 -0400

    Internationalizing Flutter apps, user’s preferred language

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:components_app/src/routes/routes.dart';
 import 'package:components_app/src/pages/alert_page.dart';
@@ -12,6 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Spanish, no country code
+      ],
+
       debugShowCheckedModeBanner: false,
       title: 'Components App',
       // home: HomePage(),
